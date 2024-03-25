7747c61df40494d50d85bb874a3bc5f141372358
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-25 16:36:28 +0100

    feat: manage create and store functions

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["title","image","price","series","sale_date","description"] ;
 }
