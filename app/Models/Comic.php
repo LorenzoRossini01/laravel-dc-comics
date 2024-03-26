d966109738cdd258e8eafebbea6cef01d8dc0fad
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-26 14:35:41 +0100

    feat: create a getter for price

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -10,4 +10,8 @@ class Comic extends Model
     use HasFactory;
 
     protected $fillable = ["title","image","price","series","sale_date","description"] ;
+
+    public function getPrice(){
+        return $this->price." $";
+    }
 }
